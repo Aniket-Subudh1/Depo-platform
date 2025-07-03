80a79484f80b3fc75ba4429e5ea3345c0bf26514
Aniket Subudhi <aniketsubudhi00@gmail.com>
2025-07-03 18:11:51 +0530

    fix

---
+++ apps/deploylite/app/project/[project]/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Projecthome from '@/utils/Project/Projecthome'
+import DatabaseComp from '@/utils/Project/Database'
+import WebBuilder from '@/utils/Project/WebBuilder'
+import VirtualSpace from '@/utils/Project/VirtualSpace'
+
+interface PageProps {
+  params: Promise<{ project: string }>
+}
+
+const page = async ({ params }: PageProps) => {
+  const { project } = await params
+  
+  return (
+    <div>
+        {project === "database" && <DatabaseComp/>}
+        {project === "app-platform" && <Projecthome name={project} />}
+        {project === "webbuilder" && <WebBuilder/>}
+        {project === "virtualspace" && <VirtualSpace/>}
+        {project === "storage" && <WebBuilder/>}
+    </div>
+  )
+}
+
+export default page
